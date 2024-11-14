39536a9d665d3678177e3d4f63c4ae2ef11bb8da
waldemar10 <waldemar.ju98@gmail.com>
2024-11-14 12:51:06 +0100

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,15 +5,23 @@ function Footer() {
     <>
       <footer>
         <p>
-          Website selbstständig erstellt mit {" "}
-          <a href="https://github.com/waldemar10/homepage" target="_blank" rel="noreferrer">
+          Website
+          {" "}
+          <a
+            href="https://github.com/waldemar10/homepage"
+            target="_blank"
+            rel="noreferrer">
             
-            React
+            selbstständig
           </a>
+          {" "}
+          erstellt mit React
         </p>
-       
-        <p><a href="/privacyPolicy">Datenschutzerklärung</a>{" "}&{" "}
-        <a href="/impressum">Impressum</a></p>
+
+        <p>
+          <a href="/privacyPolicy">Datenschutzerklärung</a> &{" "}
+          <a href="/impressum">Impressum</a>
+        </p>
         <p>© 2023 - {year}</p>
       </footer>
     </>
