cb506d1ace0f23480893e66f28ce59069b210058
Waldemar Justus <waldemar.ju98@gmail.com>
2025-04-19 15:36:44 +0200

    Fix: yoga logo color

--- frontend/src/components/projects/Yoga.js
+++ frontend/src/components/projects/Yoga.js
@@ -70,7 +70,7 @@ function Yoga() {
       </p>
 
       <svg
-        className="project__logo"
+        className="project__logo project__logo--yoga"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 200 300">
         <g>
